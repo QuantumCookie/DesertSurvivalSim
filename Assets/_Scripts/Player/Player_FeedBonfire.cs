72ea04d46cd790885e567c8caaa157aa25006cfd
QuantumCookie <csksethu111@gmail.com>
2020-05-16 21:38:50 +0530

    Minor

---
+++ Assets/_Scripts/Player/Player_FeedBonfire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_FeedBonfire : MonoBehaviour
+{
+    private Player_DetectItem itemDetector;
+    private Player_Inventory inventory;
+    private GameManager_Master gameManagerMaster;
+
+    public BaseObject wood;
+    public GameObject balloon, balloonParent;
+    
+    private void Start()
+    {
+        itemDetector = GetComponent<Player_DetectItem>();
+        inventory = GetComponent<Player_Inventory>();
+        gameManagerMaster = GameObject.FindGameObjectWithTag("GameManager").GetComponent<GameManager_Master>();
+    }
+
+    private void Update()
+    {
+        if(gameManagerMaster.isGamePaused || gameManagerMaster.isGameOver)return;
+        
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (itemDetector.collider)
+            {
+                Bonfire b = itemDetector.collider.GetComponent<Bonfire>();
+                if (b)
+                {
+                    if (inventory.GetTotalItemCount(wood) > 0)
+                    {
+                        inventory.RemoveItem(wood, 1);
+                        b.Refuel(1);
+                        Balloon text = Instantiate(balloon, Camera.main.WorldToScreenPoint(transform.position), Quaternion.identity, balloonParent.transform).GetComponent<Balloon>();
+                        text.SetText("-1 " + wood.name);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Assets/_Scripts/Utility/Bonfire.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonfire : MonoBehaviour
+{
+    [SerializeField] private float burnRate = 0.01f;
+    [SerializeField] private ParticleSystem fire;
+    [SerializeField] private Light light;
+    
+    private float maxWood = 10f;
+    private float wood = 1f;
+
+    private bool isActive = true;
+
+    private void Start()
+    {
+        isActive = true;
+    }
+
+    private void Update()
+    {
+        Burn();
+    }
+
+    private void Burn()
+    {
+        if (!isActive) return;
+        
+        wood = Mathf.Max(0, wood - burnRate * Time.deltaTime);
+        if (Mathf.CeilToInt(wood) == 0)
+        {
+            ToggleFire();
+        }
+    }
+
+    public void Refuel(float amount)
+    {
+        wood = Mathf.Min(maxWood, wood + amount);
+        if (!isActive) ToggleFire();
+    }
+
+    private void ToggleFire()
+    {
+        fire.gameObject.SetActive(!fire.gameObject.activeSelf);
+        light.gameObject.SetActive(!light.gameObject.activeSelf);    
+        isActive = !isActive;
+    }
+}
