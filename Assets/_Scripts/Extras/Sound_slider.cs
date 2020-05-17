68e6602ad7b3abcd88a1109bbe97efa86ed4d7cc
Daxxboii <64298070+Daxxboii@users.noreply.github.com>
2020-05-17 16:21:39 +0530

    Settings menu done

---
+++ Assets/_Scripts/Extras/Sound_slider.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Sound_slider : MonoBehaviour
+{
+  public Slider Vol_slider;
+  private AudioSource[] Main_Audio;
+  private GameObject source;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vol_slider = this.gameObject.GetComponent<Slider>();
+        source = GameObject.Find("Audio manager");
+        Main_Audio = source.transform.GetComponents<AudioSource>();
+        Vol_slider.value = Main_Audio[0].volume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+      Main_Audio[0].volume = Vol_slider.value;
+      if(Main_Audio[1] != null){
+        Main_Audio[1].volume = Vol_slider.value;
+      }
+    }
+}
--- Assets/_Scripts/MainMenu/MainMenuManager.cs
+++ Assets/_Scripts/MainMenu/MainMenuManager.cs
@@ -5,9 +5,12 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+public Canvas Button_Holder;
+
     public void StartGame()
     {
         SceneManager.LoadScene(1);
+
     }
 
     public void QuitGame()
@@ -18,4 +21,28 @@ public class MainMenuManager : MonoBehaviour
         Application.Quit();
 #endif
     }
+    public void Instructions(){
+          var parameters = new LoadSceneParameters(LoadSceneMode.Additive);
+         SceneManager.LoadScene("Instructions",parameters);
+            Disable_Buttons();
+        }
+    public void Settings(){
+        var parameters = new LoadSceneParameters(LoadSceneMode.Additive);
+         SceneManager.LoadScene("Settings",parameters);
+          Disable_Buttons();
+            }
+    public void Credits(){
+        var parameters = new LoadSceneParameters(LoadSceneMode.Additive);
+         SceneManager.LoadScene("Credits",parameters);
+         Disable_Buttons();
+
+                }
+   private void Disable_Buttons(){
+     Button_Holder.enabled = false;
+   }
+   void Update(){
+       if (Input.GetKeyDown(KeyCode.Escape)){
+     Button_Holder.enabled = true;
+   }
+}
 }
